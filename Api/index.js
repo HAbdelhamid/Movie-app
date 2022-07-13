4e31e071b5b30f0f4baca9b78266d40c2f6f4a06
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2022-07-13 12:01:12 +0100

    Add: api and admin functionality

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product");
+
 
 dotenv.config()
 
@@ -12,8 +15,10 @@ mongoose
     .catch((err) => {console.log(err);});
 
     app.use(express.json());
-
+    app.use("/api/auth", authRoute);
     app.use("/api/users", userRoute);
+    app.use("/api/products", productRoute);
+    
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server in runing")
